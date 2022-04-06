c3cb9bee299f68b32254bc449ab03cd38036425d
Hugo Frota <herbtyh@gmail.com>
2022-04-06 16:37:01 -0300

    Adicionar rodapé
    - Adicionar bootstrap personalizado
    - Adicionar rodapé

--- project/template/index.php
+++ project/template/index.php
@@ -14,9 +14,6 @@
   <meta name="msapplication-TileColor" content="#eeeeee">
   <meta name="theme-color" content="#ffffff">
 
-  <!-- bootstrap 4 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
   <!-- style -->
   <link rel="stylesheet" href="assets/css/style.min.css">
 
@@ -29,7 +26,7 @@
   <title>Paladino - Web base</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light mx-lg-5 paladin-navbar">
+  <nav class="navbar navbar-expand-lg navbar-light mx-lg-5 paladin-navbar bg-white shadow-sm">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <img src="./assets/images/paladinoH.png" alt="">
@@ -67,9 +64,32 @@
       </div>
     </div>
   </nav>
+
+  <footer>
+    <div class="footer bg-light">
+      <div class="footer__copy bg-dark text-light py-2">
+        <div class="container">
+          <p>Paladino © 2022. Alguns direitos reservados.</p> 
+        </div>
+      </div>
+      <div class="container">
+        <div class="footer__content pt-2 text-primary fw-bold">
+          <p>Menu</p>
+          <div class="footer__links fw-normal mt-2 pb-4 text-dark">
+            <?php for($i=0; $i<9; $i++): ?>
+              <a href="#">Item 0<?php echo $i?></a>
+            <?php endfor;?>
+          </div>
+          <div class="footer_my fw-normal fs-6 text-dark text-center py-3 border-top border-dark">
+            <a href="#">Hugo Codes</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
   
   <!-- Importações de scripts: -->
   <!-- bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
