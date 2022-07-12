79bd5b56e882725a9ac12a4455d43f1ead0535fb
Hugo Frota <herbtyh@gmail.com>
2022-07-12 15:21:04 -0300

    Adicionar icones
    - Adicionar icones

--- project/template/index.php
+++ project/template/index.php
@@ -17,6 +17,9 @@
   <!-- style -->
   <link rel="stylesheet" href="assets/css/style.min.css">
 
+  <!-- Feather icons -->
+  <script src="https://unpkg.com/feather-icons"></script>
+
   <!-- fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -89,6 +92,9 @@
   </footer>
   
   <!-- Importações de scripts: -->
+  <script>
+    feather.replace()
+  </script>
   <!-- bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
